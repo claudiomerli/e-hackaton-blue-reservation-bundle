e50c599f862419b95d4128f602da12d50cc212c9
Claudio Merli <cladlomerli99@gmail.com>
2022-12-15 09:13:25 +0100

    renamed user service in reservation-service

--- microservices/reservation-service/src/main/java/org/entando/hackaton/controller/UserController.java
+++ microservices/reservation-service/src/main/java/org/entando/hackaton/controller/UserController.java
@@ -24,19 +24,19 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/timestamp")
-    @PreAuthorize("hasAnyAuthority('user-service-role')")
+    @PreAuthorize("hasAnyAuthority('reservation-service-role')")
     public @ResponseBody Map<String, String> timestamp() {
         return Map.of("timestamp", new Date().toString());
     }
 
     @GetMapping("/users")
-    @PreAuthorize("hasAnyAuthority('user-service-role')")
+    @PreAuthorize("hasAnyAuthority('reservation-service-role')")
     public @ResponseBody List<User> getUsers() {
         return userService.getAllUsers();
     }
 
     @PostMapping("/users")
-    @PreAuthorize("hasAnyAuthority('user-service-role')")
+    @PreAuthorize("hasAnyAuthority('reservation-service-role')")
     public @ResponseBody User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
